967d4c8f1c525bf43e8f1b0f077131dffedf5ab9
swaibu <rumbiihas@gmail.com>
2019-10-28 15:57:13 +0300

    feature(signup):user can register

---
+++ app/models/loans.js
@@ -0,0 +1,50 @@
+const loans = [
+    {
+        id: 1,
+        user: "khalimah.com",
+        createdOn: 21-april-2018,
+        status:"approved",
+        repaid: true,
+        tenor:9,
+        amount: 1500000,
+        paymentInstallments: 20000,
+        balance: 1480000,
+        interest: 70000
+    },
+    {
+        id: 2,
+        user: "kkauthar.com",
+        createdOn: 21-april-2018,
+        status:"approved",
+        repaid: true,
+        tenor:9,
+        amount: 1400000,
+        paymentInstallments: 20000,
+        balance: 1480000,
+        interest: 70000
+    },
+    {
+        id: 3,
+        user: "aammaar.com",
+        createdOn: 21-april-2018,
+        status:"approved",
+        repaid: true,
+        tenor:9,
+        amount: 2000000,
+        paymentInstallments: 120000,
+        balance: 1480000,
+        interest: 70000
+    },
+    {
+        id: 4,
+        user: "rswaibu.com",
+        createdOn: 21-april-2018,
+        status:"pending",
+        repaid: false,
+        tenor:9,
+        amount: 1500000,
+        paymentInstallments:20000,
+        balance: 1500000,
+        interest: 70000
+    },
+];
---
+++ app/models/users.js
@@ -0,0 +1,42 @@
+const users = [
+    {
+    id: 1,
+    email: "khalimah@.com",
+    firstName: "katusabe",
+    lastName: "halimah",
+    pasword: "hally",
+    address: "kampala",
+    status: "verified",
+    isAdmin: true
+    },
+    {
+        id: 2,
+        email: "kkauthar@.com",
+        firstName: "amuha",
+        lastName: "kauthar",
+        pasword: "mom",
+        address: "kampala",
+        status: "unverified",
+        isAdmin: false
+        },
+        {
+            id: 3,
+            email: "aammaar@.com",
+            firstName: "muuhangi",
+            lastName: "ammaar",
+            pasword: "sill",
+            address: "kampala",
+            status: "unverified",
+            isAdmin: false
+            },
+            {
+                id: 4,
+                email: "rswaibu@.com",
+                firstName: "rumbiiha",
+                lastName: "swaibu",
+                pasword: "kat",
+                address: "kampala",
+                status: "unverified",
+                isAdmin: false
+                }
+];
